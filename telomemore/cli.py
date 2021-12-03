55c417f176b6d63e1d0009ebc0aee4e8acc2f20e
willros <william.rosenbaum88@gmail.com>
2021-12-03 15:20:27 +0100

    small change

--- telomemore/cli.py
+++ telomemore/cli.py
@@ -4,6 +4,12 @@ from telomemore.programs import NobarcodeProgramTelomemore, BarcodeProgramTelome
 from telomemore.barcodes import Barcodes
 from telomemore.telomemore import TeloMemore
 
+#COPY
+from telomemore.filehandler_copy import Files_copy
+from telomemore.programs_copy import NobarcodeProgramTelomemore_copy, BarcodeProgramTelomemore_copy
+from telomemore.barcodes import Barcodes
+from telomemore.telomemore_copy import TeloMemore_copy
+
 @click.group()
 def cli():
     '''WELCOME TO teloMeMore'''
@@ -52,7 +58,46 @@ def count(inputs, barcodes, pattern, cutoff, output):
         telomemore.run_program()
     
     
+@cli.command()
+@click.option('--inputs', '-i', type=str, required=True, help='input folder or file for TeloMeMore')
+@click.option('--pattern', '-p', type=str, required=True, default='CCCTAA', help='pattern for searching the bam files')
+@click.option('--barcodes', '-bc', type=str, required=False, default=None, help='barcode file or folder is barcode file exists')
+@click.option('--cutoff', '-c', type=int, required=False, default=3, help='cutoff for which telomermore count occurance of pattern as telomere read')
+@click.option('--output', '-o', type=str, required=False, default=None, help='specify customize output folder if wanted')
+def count_copy(inputs, barcodes, pattern, cutoff, output):
     
+    '''Count the occurances of telomere read in each cell. The telomere is defined by the user, as a read
+    which contains the pattern a number of times above the specified cutoff.
+    The program goes through all bam files in the input folder and stores the results as csv files in the output
+    folder. A cell is defined by the cell barcode in the bam file.
+
+    Required arguments: 
+    
+    inputs: a folder or file
+    
+    pattern: a pattern which TeloMemore searches the bamfile for. 'CCCTAA' as default. 
+        
+    Optional arguments:
+    
+    cutoff: integer for which Telomemore uses as cutoff for counting read as telomere. Default = 3.
+    
+    barcodes: file or folder with barcode files.
+    
+    output: Specify folder to which the count files should be written. Default is the same folder as input. 
+    
+    '''
+
+    files = Files_copy(inputs)
+    
+    if barcodes is not None:
+        program = BarcodeProgramTelomemore_copy()
+        barcodes = Barcodes(barcodes)
+        telomemore = TeloMemore_copy(pattern=pattern, files=files, program=program, cutoff=cutoff, barcode=barcodes, output_dir=output)
+        telomemore.run_program()
+    else:
+        program = NobarcodeProgramTelomemore_copy()
+        telomemore = TeloMemore_copy(pattern=pattern, files=files, program=program, cutoff=cutoff, output_dir=output)
+        telomemore.run_program()
     
     
   
--- telomemore/programs_copy.py
+++ telomemore/programs_copy.py
@@ -32,14 +32,15 @@ class ProgramTelomemore(ABC):
         counts = re.findall(pattern, sequence)
         return len(counts)
     
+    
 class NobarcodeProgramTelomemore_copy(ProgramTelomemore):
     
     def telomere_count(self, sam: Path, cutoff: int, pattern: str) -> dict:
-        pysam = pysam.AlignmentFile(sam, 'rb')
+        sam_file = pysam.AlignmentFile(sam, 'rb')
         telomeres_cells = defaultdict(Count)
         missed_barcodes = 0
 
-        for read in tqdm(pysam):
+        for read in tqdm(sam_file):
             try:
                 cb = read.get_tag('CB')
                 seq = read.seq
@@ -69,12 +70,12 @@ class BarcodeProgramTelomemore_copy(ProgramTelomemore):
         '''Counts number of telomeres from barcode file and returns the total reads per cells, 
         telomeres per cells and reads with missed barcodes. '''
         
-        pysam = pysam.AlignmentFile(sam, 'rb')
+        sam_file = pysam.AlignmentFile(sam, 'rb')
         barcode = pd.read_csv(barcode, header=None, delimiter='\t', names=['bc'])
         telomeres_cells = {x: Count() for x in barcode['bc'].to_list()}
         missed_barcodes = 0
 
-        for read in tqdm(pysam):
+        for read in tqdm(sam_file):
             try:
                 cb = read.get_tag('CB')
                 seq = read.seq
